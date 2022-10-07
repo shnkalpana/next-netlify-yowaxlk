1df19da88dc6c230f606874a31bef0db8f8f9836
Sahan Kalpana <shnkalpana@gmail.com>
2022-10-07 19:45:05 +0530

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+      <div>
+        <title>Yowax.lk</title>
+        <main className=" bg-white px-10">
+          <section className="min-h-screen py-5">
+            <nav className="py-8 mb-12 flex justify-between">
+            <h1> Yowax.lk</h1>
+              <ul className="flex pr-2 items-center">
+                <li>About</li>
+                <li className="ml-8">Products</li>
+              </ul>
+            </nav>
+            <div>
+              <Image src="/surf.jpg" className=" h-40 w-auto"/>
+            </div>
+          </section>
+          
+        </main>
+      </div>
+      
   )
 }
