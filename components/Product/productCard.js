5e1b258ee9a6ee56ce6cd1ba4c78cc3e2e08c5e2
Sahan Kalpana <shnkalpana@gmail.com>
2023-05-24 06:32:52 +0530

    cange

--- components/Product/productCard.js
+++ components/Product/productCard.js
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 const ProductCard = ({ productName, productImage, description }) => {
   return (
-    <div className=" grid-cols-3 border rounded-xl w-80 h-96 px-2">
+    <div className=" grid-cols-3 border rounded-xl w-80 h-96 px-1">
       <img
-        className=" rounded-lg mt-3 flex mx-14"
+        className=" rounded-lg mt-3 flex mx-12"
         src={productImage}
         alt={productName}
         width={100}
         height={100}
       />
       <div className=" text-center mt-1 text-[#e9ecf0]">{productName}</div>
-      <p className=" text-sm text-center text-[#e9ecf0]">{description}</p>
+      <p className=" text-xs text-center text-[#e9ecf0]">{description}</p>
     </div>
   );
 };
