434067e225cf77549d1bb04aefdb0cfd378f9546
Sahan Kalpana <shnkalpana@gmail.com>
2023-05-24 06:46:36 +0530

    justifyCenter

--- components/Product/productCard.js
+++ components/Product/productCard.js
@@ -4,13 +4,15 @@ import Image from "next/image";
 const ProductCard = ({ productName, productImage, description }) => {
   return (
     <div className=" grid-cols-3 border rounded-xl w-80 h-96 px-1">
-      <img
-        className=" rounded-lg mt-3 flex justify-between"
-        src={productImage}
-        alt={productName}
-        width={100}
-        height={100}
-      />
+      <div className="flex justify-center">
+        <img
+          className=" rounded-lg mt-3 "
+          src={productImage}
+          alt={productName}
+          width={100}
+          height={100}
+        />
+      </div>
       <div className=" text-center mt-1 text-[#e9ecf0]">{productName}</div>
       <p className=" text-xs text-center text-[#e9ecf0]">{description}</p>
     </div>
